406d8ec6ec8eb0cf1c7935fcca41cbef326fed9a
Dectre <amirali.dh84@gmail.com>
2024-12-05 13:39:20 +0330

    Relative Path problem has fixed. Read FixClionAbsolutePathBug.MD file.

--- src/Manual.h
+++ src/Manual.h
@@ -11,6 +11,6 @@ using namespace std;
 const int windowWidth = 1080;
 const int windowHeight = 720;
 
-const string BACKGROUND = "/media/dectre/New Volume/codes/C++/AP/SheepFight/sprites/background/background.png";
+const string BACKGROUND = "sprites/background/background.png";
 
 #endif //SHEEPFIGHT_MANUAL_H
